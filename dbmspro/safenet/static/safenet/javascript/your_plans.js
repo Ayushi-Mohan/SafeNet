3f6580a92565e46d3caa57d5db0cdf5f27783707
Ayushi Mohan <mishimohan16@gmail.com>
2019-11-08 18:43:11 +0530

    yourplanhtml

---
+++ dbmspro/safenet/static/safenet/javascript/your_plans.js
@@ -0,0 +1,265 @@
+function myCheck1Function() {
+    // Get the checkbox
+    var checkBox1 = document.getElementById("customCheck1");
+    // Get the switch
+    var divSwitch1 = document.getElementById("divSwitch1");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox1.checked == true){
+        divSwitch1.style.display = "block";
+    } else {
+        divSwitch1.style.display = "none";
+    }
+}
+
+function mySwitch1Function() {
+    // Get the switch
+    var  Switch1 = document.getElementById("customSwitch1");
+    // Get the redirection url
+    var divText1 = document.getElementById("divText1");
+
+    // If the switch is checked, display the url field
+    if (Switch1.checked == true){
+        divText1.style.display = "block";
+    } else {
+        divText1.style.display = "none";
+    }
+}
+
+function myCheck2Function() {
+    // Get the checkbox
+    var checkBox2 = document.getElementById("customCheck2");
+    // Get the switch
+    var divSwitch2 = document.getElementById("divSwitch2");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox2.checked == true){
+        divSwitch2.style.display = "block";
+    } else {
+        divSwitch2.style.display = "none";
+    }
+}
+
+function mySwitch2Function() {
+    // Get the switch
+    var  Switch2 = document.getElementById("customSwitch2");
+    // Get the redirection url
+    var divText2 = document.getElementById("divText2");
+
+    // If the switch is checked, display the url field
+    if (Switch2.checked == true){
+        divText2.style.display = "block";
+    } else {
+        divText2.style.display = "none";
+    }
+}
+
+function myCheck3Function() {
+    // Get the checkbox
+    var checkBox3 = document.getElementById("customCheck3");
+    // Get the switch
+    var divSwitch3 = document.getElementById("divSwitch3");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox3.checked == true){
+        divSwitch3.style.display = "block";
+    } else {
+        divSwitch3.style.display = "none";
+    }
+}
+
+function mySwitch3Function() {
+    // Get the switch
+    var  Switch3 = document.getElementById("customSwitch3");
+    // Get the redirection url
+    var divText3 = document.getElementById("divText3");
+
+    // If the switch is checked, display the url field
+    if (Switch3.checked == true){
+        divText3.style.display = "block";
+    } else {
+        divText3.style.display = "none";
+    }
+}
+
+function myCheck4Function() {
+    // Get the checkbox
+    var checkBox4 = document.getElementById("customCheck4");
+    // Get the switch
+    var divSwitch4 = document.getElementById("divSwitch4");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox4.checked == true){
+        divSwitch4.style.display = "block";
+    } else {
+        divSwitch4.style.display = "none";
+    }
+}
+
+function mySwitch4Function() {
+    // Get the switch
+    var  Switch4 = document.getElementById("customSwitch4");
+    // Get the redirection url
+    var divText4 = document.getElementById("divText4");
+
+    // If the switch is checked, display the url field
+    if (Switch4.checked == true){
+        divText4.style.display = "block";
+    } else {
+        divText4.style.display = "none";
+    }
+}
+
+function myCheck5Function() {
+    // Get the checkbox
+    var checkBox5 = document.getElementById("customCheck5");
+    // Get the switch
+    var divSwitch5 = document.getElementById("divSwitch5");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox5.checked == true){
+        divSwitch5.style.display = "block";
+    } else {
+        divSwitch5.style.display = "none";
+    }
+}
+
+function mySwitch5Function() {
+    // Get the switch
+    var  Switch5 = document.getElementById("customSwitch5");
+    // Get the redirection url
+    var divText5 = document.getElementById("divText5");
+
+    // If the switch is checked, display the url field
+    if (Switch5.checked == true){
+        divText5.style.display = "block";
+    } else {
+        divText5.style.display = "none";
+    }
+}
+
+function myCheck6Function() {
+    // Get the checkbox
+    var checkBox6 = document.getElementById("customCheck6");
+    // Get the switch
+    var divSwitch6 = document.getElementById("divSwitch6");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox6.checked == true){
+        divSwitch6.style.display = "block";
+    } else {
+        divSwitch6.style.display = "none";
+    }
+}
+
+function mySwitch6Function() {
+    // Get the switch
+    var  Switch6 = document.getElementById("customSwitch6");
+    // Get the redirection url
+    var divText6 = document.getElementById("divText6");
+
+    // If the switch is checked, display the url field
+    if (Switch6.checked == true){
+        divText6.style.display = "block";
+    } else {
+        divText6.style.display = "none";
+    }
+}
+
+function myCheck7Function() {
+    // Get the checkbox
+    var checkBox7 = document.getElementById("customCheck7");
+    // Get the switch
+    var divSwitch7 = document.getElementById("divSwitch7");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox7.checked == true){
+        divSwitch7.style.display = "block";
+    } else {
+        divSwitch7.style.display = "none";
+    }
+}
+
+function mySwitch7Function() {
+    // Get the switch
+    var  Switch7 = document.getElementById("customSwitch7");
+    // Get the redirection url
+    var divText7 = document.getElementById("divText7");
+
+    // If the switch is checked, display the url field
+    if (Switch7.checked == true){
+        divText7.style.display = "block";
+    } else {
+        divText7.style.display = "none";
+    }
+}
+
+function myCheck8Function() {
+    // Get the checkbox
+    var checkBox8 = document.getElementById("customCheck8");
+    // Get the switch
+    var divSwitch8 = document.getElementById("divSwitch8");
+
+    // If the checkbox is checked, display the switch
+    if (checkBox8.checked == true){
+        divSwitch8.style.display = "block";
+    } else {
+        divSwitch8.style.display = "none";
+    }
+}
+
+function mySwitch8Function() {
+    // Get the switch
+    var  Switch8 = document.getElementById("customSwitch8");
+    // Get the redirection url
+    var divText8 = document.getElementById("divText8");
+
+    // If the switch is checked, display the url field
+    if (Switch8.checked == true){
+        divText8.style.display = "block";
+    } else {
+        divText8.style.display = "none";
+    }
+}
+
+function mySwitchCust1Function() {
+    // Get the switch
+    var  SwitchCust1 = document.getElementById("customSwitchCust1");
+    // Get the redirection url
+    var divTextCust1 = document.getElementById("divTextCust1");
+
+    // If the switch is checked, display the url field
+    if (SwitchCust1.checked == true){
+        divTextCust1.style.display = "block";
+    } else {
+        divTextCust1.style.display = "none";
+    }
+}
+
+function mySwitchCust2Function() {
+    // Get the switch
+    var  SwitchCust2 = document.getElementById("customSwitchCust2");
+    // Get the redirection url
+    var divTextCust2 = document.getElementById("divTextCust2");
+
+    // If the switch is checked, display the url field
+    if (SwitchCust2.checked == true){
+        divTextCust2.style.display = "block";
+    } else {
+        divTextCust2.style.display = "none";
+    }
+}
+
+function mySwitchCust3Function() {
+    // Get the switch
+    var  SwitchCust3 = document.getElementById("customSwitchCust3");
+    // Get the redirection url
+    var divTextCust3 = document.getElementById("divTextCust3");
+
+    // If the switch is checked, display the url field
+    if (SwitchCust3.checked == true){
+        divTextCust3.style.display = "block";
+    } else {
+        divTextCust3.style.display = "none";
+    }
+}
